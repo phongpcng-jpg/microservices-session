1da2900d5ce0ae1a3a6820e8d21075379da9b618
Thomas Phong <ino.thomas@outlook.com>
2026-09-08 17:17:13 +0700

    feat: initialize customer server
    - Session: 03
    - Exercise: 01

---
+++ customer-service/src/test/java/io/github/nguyenquephong13062003/customer_service/CustomerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.nguyenquephong13062003.customer_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
