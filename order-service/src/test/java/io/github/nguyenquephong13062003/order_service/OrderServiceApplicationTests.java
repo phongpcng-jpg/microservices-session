e5a2743fdd1db770fa579f1649cb44544a798c74
Thomas Phong <ino.thomas@outlook.com>
2026-09-08 17:18:04 +0700

    feat: initialize order server
    - Session: 03
    - Exercise: 01

---
+++ order-service/src/test/java/io/github/nguyenquephong13062003/order_service/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.nguyenquephong13062003.order_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
