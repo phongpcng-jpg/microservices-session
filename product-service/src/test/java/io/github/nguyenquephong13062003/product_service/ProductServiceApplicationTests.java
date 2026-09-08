13d7759c85d57d394de80f98f3a10c73dac413da
Thomas Phong <ino.thomas@outlook.com>
2026-09-08 17:17:38 +0700

    feat: initialize product server
    - Session: 03
    - Exercise: 01

---
+++ product-service/src/test/java/io/github/nguyenquephong13062003/product_service/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.nguyenquephong13062003.product_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
