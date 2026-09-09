ecaa09ccf4f8929fef7c31800284b5b853761896
Thomas Phong <ino.thomas@outlook.com>
2026-09-09 12:38:21 +0700

    feat: setup Eureka discovery server for customer server
    - Session: 04
    - Exercise: 02

--- customer-service/src/main/java/io/github/nguyenquephong13062003/customer_service/CustomerServiceApplication.java
+++ customer-service/src/main/java/io/github/nguyenquephong13062003/customer_service/CustomerServiceApplication.java
@@ -2,12 +2,14 @@ package io.github.nguyenquephong13062003.customer_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * The main class for the Customer Service application.
  * It serves as the entry point for the Spring Boot application.
  */
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CustomerServiceApplication {
 
 	/**
